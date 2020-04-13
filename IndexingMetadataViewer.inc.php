7cd838821c4c82fa64a64b8e18cec1aa7a98b69d
mgantola <mg.antola@gmail.com>
2020-04-13 20:38:17 -0300

    Nueva version de prueba.

--- IndexingMetadataViewer.inc.php
+++ IndexingMetadataViewer.inc.php
@@ -1,42 +1,100 @@
 <?php
 
+/**
+ * @file plugins/generic/iterSubmission/IterSubmission.inc.php
+ *
+ * Copyright (c) 2014-2016 Simon Fraser University Library
+ * Copyright (c) 2003-2016 John Willinsky
+ * Distributed under the GNU GPL v2. For full terms see the file docs/COPYING.
+ *
+ * @class IterSubmission
+ * @ingroup plugins_generic_iterSubmission
+ *
+ * @brief This adds new fields to the submission and makes
+ * those available as metadata data and form operations.
+ */
+
 import('lib.pkp.classes.plugins.GenericPlugin');
+// import('lib.pkp.classes.submission.SubmissionMetadataFormImplementation');
+// import('classes.submission.SubmissionMetadataFormImplementation');
 
-class IndexingMetadataViewer extends GenericPlugin  {
+class IterSubmission extends GenericPlugin {
 	/**
-	 * @copydoc Plugin::register()
+	 * Constructor.
+	 * @param $parentForm Form A form that can use this form.
 	 */
-    public function register($category, $path, $mainContextId = NULL) { 
-		// Register the plugin even when it is not enabled
-		$success = parent::register($category, $path, $mainContextId);
-
+	 
+	 /*
+	function IterSubmission($parentForm = null) {
+		// parent::SubmissionMetadataFormImplementation($parentForm);
+	}
+	*/
+	
+	function register($category, $path) {
+		$success = parent::register($category, $path);
 		if ($success && $this->getEnabled()) {
-			// Do something when the plugin is enabled
+			//
 		}
+		return $success;	
+	}
+	
+	/**
+	 * Determine the plugin sequence. Overrides parent so that
+	 * the plugin will be displayed during install.
+	 */
+	function getSeq() {
+		if (!Config::getVar('general', 'installed')) return 1;
+		return 0;
+	}	
 
-		return $success;
+	function getName() {
+		return 'Indexing Metadata';
 	}
 
+	function getDisplayName() {
+		return 'Indexing Metadata';
+	}
 
+	function getDescription() {
+		return 'Indexing Metadata';
+	}	
 
 	/**
-	 * Provide a name for this plugin
-	 *
-	 * The name will appear in the Plugin Gallery where editors can
-	 * install, enable and disable plugins.
+	 * Get the filename of the ADODB schema for this plugin.
+	 * @return string Full path and filename to schema descriptor.
 	 */
-	public function getDisplayName() {
-		return __('plugins.generic.iMV.title');
+	function getInstallSchemaFile() {
+		return $this->getPluginPath() . '/schema.xml';
 	}
 
 	/**
-	 * Provide a description for this plugin
-	 *
-	 * The description will appear in the Plugin Gallery where editors can
-	 * install, enable and disable plugins.
+	 * Hook callback function for TemplateManager::display
+	 * @param $hookName string
+	 * @param $args array
+	 * @return boolean
 	 */
-	public function getDescription() {
-		return __('plugins.generic.iMV.description');
+	function callback($hookName, $args) {
+		/*
+		// Only pages requests interest us here
+		$request =& Registry::get('request');
+		if (!is_a($request->getRouter(), 'PKPPageRouter')) return null;
+		$page = Request::getRequestedPage();
+		$op = Request::getRequestedOp();
+		switch ("$page/$op") {
+			case 'article/view':
+				$templateManager =& $args[0];
+				$additionalHeadData = $templateManager->get_template_vars('additionalHeadData');
+				$templateManager->assign(
+					'additionalHeadData',
+					$templateManager->get_template_vars('additionalHeadData') .
+					'<script async defer src="//hypothes.is/embed.js"></script>'
+				);
+				break;
+		}
+		*/
+		return false;
 	}
 	
 }
+
+?>
--- index.php
+++ index.php
@@ -3,16 +3,16 @@
  * @defgroup plugins_generic_iterSubmission Iter Submission plugin
  */
 /**
- * @file plugins/generic/iterSubmission/index.php
+ * @file plugins/generic/IndexingMetadataViewer/index.php
  *
  * Copyright (c) 2014-2016 Simon Fraser University Library
  * Copyright (c) 2003-2016 John Willinsky
  * Distributed under the GNU GPL v2. For full terms see the file docs/COPYING.
  *
  * @ingroup plugins_generic_iterSubmission
- * @brief Wrapper for "Iter Submission" plugin.
+ * @brief Wrapper for "IndexingMetadataViewer" plugin.
  *
  */
-require_once('IterSubmission.inc.php');
-return new IterSubmission();
+require_once('IndexingMetadataViewer.inc.php');
+return new IndexingMetadataViewer();
 ?>
