44e28a443382fbf6c8e9c854456a4bea3fa310d6
mgantola <mg.antola@gmail.com>
2020-04-13 21:18:13 -0300

    b

--- IndexingMetadataViewer.inc.php
+++ IndexingMetadataViewer.inc.php
@@ -18,7 +18,7 @@ import('lib.pkp.classes.plugins.GenericPlugin');
 // import('lib.pkp.classes.submission.SubmissionMetadataFormImplementation');
 // import('classes.submission.SubmissionMetadataFormImplementation');
 
-class IndexingMetadataViewer extends GenericPlugin {
+public class IndexingMetadataViewer extends GenericPlugin {
 	/**
 	 * Constructor.
 	 * @param $parentForm Form A form that can use this form.
@@ -30,7 +30,7 @@ class IndexingMetadataViewer extends GenericPlugin {
 	}
 	*/
 	
-	function register($category, $path, $mainContextId = NULL) {
+	public function register($category, $path, $mainContextId = NULL) {
 		$success = parent::register($category, $path);
 		if ($success && $this->getEnabled()) {
 			HookRegistry::register( 
@@ -41,34 +41,19 @@ class IndexingMetadataViewer extends GenericPlugin {
 		return $success;	
 	}
 	
-	/**
-	 * Determine the plugin sequence. Overrides parent so that
-	 * the plugin will be displayed during install.
-	 */
-	function getSeq() {
-		if (!Config::getVar('general', 'installed')) return 1;
-		return 0;
-	}	
 
-	function getName() {
+	public function getName() {
 		return 'Indexing Metadata';
 	}
 
-	function getDisplayName() {
+	public function getDisplayName() {
 		return 'Indexing Metadata';
 	}
 
-	function getDescription() {
+	public function getDescription() {
 		return 'Indexing Metadata';
 	}	
 
-	/**
-	 * Get the filename of the ADODB schema for this plugin.
-	 * @return string Full path and filename to schema descriptor.
-	 */
-	function getInstallSchemaFile() {
-		return $this->getPluginPath() . '/schema.xml';
-	}
 
 	/**
 	 * Hook callback function for TemplateManager::display
@@ -76,7 +61,7 @@ class IndexingMetadataViewer extends GenericPlugin {
 	 * @param $args array
 	 * @return boolean
 	 */
-	function callback($hookName, $args) {
+	public function callback($hookName, $args) {
 		/*
 		// Only pages requests interest us here
 		$request =& Registry::get('request');
