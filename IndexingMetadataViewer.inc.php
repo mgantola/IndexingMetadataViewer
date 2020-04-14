ffc866fcf8c8ae9554b4b6f5f4634c004cbd508b
mgantola <mg.antola@gmail.com>
2020-04-13 21:32:18 -0300

    newTest

--- IndexingMetadataViewer.inc.php
+++ IndexingMetadataViewer.inc.php
@@ -1,88 +1,35 @@
 <?php
-
-/**
- * @file plugins/generic/iterSubmission/IterSubmission.inc.php
- *
- * Copyright (c) 2014-2016 Simon Fraser University Library
- * Copyright (c) 2003-2016 John Willinsky
- * Distributed under the GNU GPL v2. For full terms see the file docs/COPYING.
- *
- * @class IterSubmission
- * @ingroup plugins_generic_iterSubmission
- *
- * @brief This adds new fields to the submission and makes
- * those available as metadata data and form operations.
- */
-
 import('lib.pkp.classes.plugins.GenericPlugin');
-// import('lib.pkp.classes.submission.SubmissionMetadataFormImplementation');
-// import('classes.submission.SubmissionMetadataFormImplementation');
-
-public class IndexingMetadataViewer extends GenericPlugin {
-	/**
-	 * Constructor.
-	 * @param $parentForm Form A form that can use this form.
-	 */
-	 
-	 /*
-	function IterSubmission($parentForm = null) {
-		// parent::SubmissionMetadataFormImplementation($parentForm);
-	}
-	*/
-	
+class IndexingMetadataViewer extends GenericPlugin {
 	public function register($category, $path, $mainContextId = NULL) {
+
+		// Register the plugin even when it is not enabled
 		$success = parent::register($category, $path);
+
 		if ($success && $this->getEnabled()) {
-			HookRegistry::register( 
-                'Templates::Manager::Index::ManagementPages', 
-                array(&$this, 'callback') 
-            );
+			// Do something when the plugin is enabled
 		}
-		return $success;	
+
+		return $success;
 	}
-	
-
-	public function getName() {
-		return 'Indexing Metadata';
-	}
-
-	public function getDisplayName() {
-		return 'Indexing Metadata';
-	}
-
-	public function getDescription() {
-		return 'Indexing Metadata';
-	}	
-
 
 	/**
-	 * Hook callback function for TemplateManager::display
-	 * @param $hookName string
-	 * @param $args array
-	 * @return boolean
+	 * Provide a name for this plugin
+	 *
+	 * The name will appear in the Plugin Gallery where editors can
+	 * install, enable and disable plugins.
 	 */
-	public function callback($hookName, $args) {
-		/*
-		// Only pages requests interest us here
-		$request =& Registry::get('request');
-		if (!is_a($request->getRouter(), 'PKPPageRouter')) return null;
-		$page = Request::getRequestedPage();
-		$op = Request::getRequestedOp();
-		switch ("$page/$op") {
-			case 'article/view':
-				$templateManager =& $args[0];
-				$additionalHeadData = $templateManager->get_template_vars('additionalHeadData');
-				$templateManager->assign(
-					'additionalHeadData',
-					$templateManager->get_template_vars('additionalHeadData') .
-					'<script async defer src="//hypothes.is/embed.js"></script>'
-				);
-				break;
-		}
-		*/
-		return false;
+	public function getDisplayName() {
+		return 'Indexing Metadata Viewer';
 	}
-	
-}
 
-?>
+	/**
+	 * Provide a description for this plugin
+	 *
+	 * The description will appear in the Plugin Gallery where editors can
+	 * install, enable and disable plugins.
+	 */
+	public function getDescription() {
+		return 'This plugin is an example created for a tutorial on how to create a plugin.';
+	}
+}
